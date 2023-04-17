c0f9f3c210d4bc3d09e4d780030f6e7de171d373
Nguyen566-spec <ducdungvt1996@gmail.com>
2023-04-17 15:24:36 +0700

    Install axios, redux, tailwindcss and others.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -72,6 +72,14 @@ const Header = () => {
                 Login
               </NavLink>
             </li>
+            <li>
+              <NavLink
+                to="/register"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+              >
+                Register
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
